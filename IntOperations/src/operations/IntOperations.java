184a2593b3b147215c086f3d51b4d656ed031244
Guinther Erich K S Anders <67793128+guinther-erich@users.noreply.github.com>
2021-11-13 14:47:38 -0300

    Upload IntOperations

---
+++ IntOperations/src/operations/IntOperations.java
@@ -0,0 +1,14 @@
+package operations;
+
+public class IntOperations {
+
+	/*Main is a self-executable method in Java*/
+	public static void main(String[] args) {
+		int exam1 = 90;
+		int exam2 = 70;
+		int exam3 = 50;
+		int exam4 = 88;
+		
+		System.out.println("Média das notas = " + (exam1 + exam2 + exam3 + exam4) / 4);
+	}
+}

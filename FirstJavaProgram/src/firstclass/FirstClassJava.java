458262998ac8839b5513d2a8c91a15c565792032
Guinther Erich K S Anders <67793128+guinther-erich@users.noreply.github.com>
2021-11-13 02:30:03 -0300

    The first Java program

---
+++ FirstJavaProgram/src/firstclass/FirstClassJava.java
@@ -0,0 +1,12 @@
+package firstclass;
+
+public class FirstClassJava {
+
+	/*Main is a self-executable method in Java*/
+	public static void main(String[] args) {
+		
+		/*Writes the text to the console*/
+		System.out.println("Vai Corinthians!");
+		System.out.println("Text written for the class on the first Treinamento Java Web FullStack program.");
+	}
+}
